e3339706e638bd9171ef67329fd4d7d0962a3dba
lianamishkat <lianamishkat@gmail.com>
2021-04-02 13:25:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import useWordGame from "./useWordGame";
 
 function App() {
+  const {
+    textBoxRef,
+    handleChange,
+    text,
+    isTimeRunning,
+    timeRemaining,
+    startGame,
+    wordCount
+  } = useWordGame(15);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>
+        <span>speed typing game</span>
+      </h2>
+      <textarea
+        onChange={handleChange}
+        value={text}
+        disabled={!isTimeRunning}
+        ref={textBoxRef}
+      />
+      <h3>time remaining: {timeRemaining}</h3>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        start game
+      </button>
+      <h3>word count: {wordCount}</h3>
     </div>
   );
 }
